abb45500512030818b22ff507c084c2353c1ba91
Beomyong Choi <dev.bychoi@gmail.com>
2016-10-20 17:26:12 +0900

    whoa, still confusing... :(

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -1,3 +1,119 @@
-from django.test import TestCase
+import datetime
 
-# Create your tests here.
+from django.utils import timezone
+from django.test import TestCase
+from django.urls import reverse
+
+from .models import Question
+
+class QuestionMethodTests(TestCase):
+
+    def test_was_published_recently_with_future_question(self):
+        """
+        pub_date가 미래면 was_published_recently()는
+        false를 반환해야 마땅합니다.
+        """
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(pub_date=time)
+        self.assertIs(future_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_old_question(self):
+        """
+        pub_date가 지난 24시간보다 이전이라면
+        was_published_recently()는 False를 반환해야 마땅합니다
+        """
+        time = timezone.now() - datetime.timedelta(days=30)
+        old_question = Question(pub_date=time)
+        self.assertIs(old_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_recent_question(self):
+        """
+        pub_date가 지난 24시간 이내라면
+        was_published_recently()는 True를 반환해야 마땅 합니다
+        """
+        time = timezone.now() - datetime.timedelta(hours=1)
+        recent_question = Question(pub_date=time)
+        self.assertIs(recent_question.was_published_recently(), True)
+
+
+def create_question(question_text, days):
+    """
+    주어진 `question_text`과 `days`로 설문을 만듭니다. `days`는 지금부터 발행일까지 날짜 수입니다
+    (음수면 과거에 발행한 설문, 양수면 아직 발행하지 않은 설문을 뜻합니다).
+    """
+    time = timezone.now() + datetime.timedelta(days=days)
+    return Question.objects.create(question_text=question_text, pub_date=time)
+
+
+class QuestionViewTests(TestCase):
+    def test_index_view_with_no_questions(self):
+        """
+        설문이 없으면 적절한 메시지가 출력되어야 마땅합니다
+        """
+        response = self.client.get(reverse('polls:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No polls are available.")
+        self.assertQuerysetEqual(response.context['latest_question_list'], [])
+
+    def test_index_view_with_a_past_question(self):
+        """
+        pub_date가 과거인 설문은 목록 페이지에 표시되어야 마땅합니다
+        """
+        create_question(question_text="Past question.", days=-30)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(
+            response.context['latest_question_list'],
+            ['<Question: Past question.>']
+        )
+
+    def test_index_view_with_a_future_question(self):
+        """
+        pub_date가 미래인 설문은 목록 페이지에 표시되지 않아야 마땅합니다
+        """
+        create_question(question_text="Future question.", days=30)
+        response = self.client.get(reverse('polls:index'))
+        self.assertContains(response, "No polls are available.")
+        self.assertQuerysetEqual(response.context['latest_question_list'], [])
+
+    def test_index_view_with_future_question_and_past_question(self):
+        """
+        발행일이 과거인 설문과 미래인 설문이 모두 있더라도 과거인 설문만 표시되어야 마땅합니다
+        """
+        create_question(question_text="Past question.", days=-30)
+        create_question(question_text="Future question.", days=30)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(
+            response.context['latest_question_list'],
+            ['<Question: Past question.>']
+        )
+
+    def test_index_view_with_two_past_questions(self):
+        """
+        설문 목록 페이지는 여러 설문을 표시할 수 있습니다
+        """
+        create_question(question_text="Past question 1.", days=-30)
+        create_question(question_text="Past question 2.", days=-5)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(
+            response.context['latest_question_list'],
+            ['<Question: Past question 2.>', '<Question: Past question 1.>']
+        )
+
+class QuestionIndexDetailTests(TestCase):
+    def test_detail_view_with_a_future_question(self):
+        """
+        pub_date가 미래이면 상세 뷰는 404 not found를 반환해야 마땅합니다
+        """
+        future_question = create_question(question_text='Future question.', days=5)
+        url = reverse('polls:detail', args=(future_question.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+
+    def test_detail_view_with_a_past_question(self):
+        """
+        pub_date가 과거인 상세 뷰는 설문 내용을 표시해야 마땅합니다
+        """
+        past_question = create_question(question_text='Past Question.', days=-5)
+        url = reverse('polls:detail', args=(past_question.id,))
+        response = self.client.get(url)
+        self.assertContains(response, past_question.question_text)
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,4 +1,54 @@
-from django.http import HttpResponse  
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.views import generic
+from django.utils import timezone
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+from .models import Choice, Question
+
+
+class IndexView(generic.ListView):
+    template_name = 'polls/Index.html'
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self):
+        """
+        (미래에 발행할 설문은 제외하고) 가장 최근에 발행된 설문 5개를 반환합니다
+        """
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
+        # return Question.objects.all()
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+    def get_queryset(self):
+        """
+        아직 발행하지 않은 설문을 제외합니다
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # 설문 폼을 다시 표시합니다.
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # POST 데이터를 성공적으로 처리한 후에는 항상 HttpResponseRedirect를
+        # 반환합니다. 그래야 사용자가 뒤로가가(Back) 버튼을 클릭했을 때 폼이
+        # 두 번 제출되는 현상이 생기지 않습니다.
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
